2c561af1511c03777a67aac450610ad83665443b
Amaurys Almanzar <amaurys.almanzar@wework.com>
2019-04-27 00:14:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,6 +34,6 @@ function preserveDecimal(a){
   return parseFloat ('2.222')
 }
 
-function preserveDecimal('2.222'){
-  return parseFloat ('a')
+function preserveDecimal(a){
+  return parseFloat ('2.222' a)
 }
